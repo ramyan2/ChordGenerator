ab69477bc1d0e70fbe3ad769e78e763b571e7e61
Ramya Nagapudi <you@yourdomain.com>
2019-04-08 16:05:19 -0500

    Created ofApp class.

---
+++ src/ofApp.cpp
@@ -0,0 +1,8 @@
+//
+//  ofApp.cpp
+//  final-project-ramyan2
+//
+//  Created by Ramya Nagapudi on 4/8/19.
+//
+
+#include "ofApp.hpp"
---
+++ src/ofApp.hpp
@@ -0,0 +1,13 @@
+//
+//  ofApp.hpp
+//  final-project-ramyan2
+//
+//  Created by Ramya Nagapudi on 4/8/19.
+//
+
+#ifndef ofApp_hpp
+#define ofApp_hpp
+
+#include <stdio.h>
+
+#endif /* ofApp_hpp */
