55ec2e59be3ca87388c34102daf240acb0980e25
Ramya Nagapudi <you@yourdomain.com>
2019-04-12 16:53:41 -0500

    Able to play the notes of an entire scale on a keyboard, by pressing the key with the desired note.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,13 +1,8 @@
 #include "ofApp.h"
-
 //--------------------------------------------------------------
 void ofApp::setup(){
     
-    my_player.load("AFlat.mp3");
-    
-    my_player.setVolume(0.5);
-    
-    my_player.play();
+   
 
 }
 
@@ -23,7 +18,67 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
+    if (key == 'c') {
+        my_player.load("C.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'C') {
+        my_player.load("C#.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'd') {
+        my_player.load("D.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'D') {
+        my_player.load("D#.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'e') {
+        my_player.load("E.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'f') {
+        my_player.load("F.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'F') {
+        my_player.load("F#.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'g') {
+        my_player.load("G.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'G') {
+        my_player.load("G#.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'a') {
+        my_player.load("A.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'A') {
+        my_player.load("A#.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
+    if (key == 'b') {
+        my_player.load("B.mp3");
+        my_player.setVolume(0.5);
+        my_player.play();
+    }
 }
 
 //--------------------------------------------------------------
